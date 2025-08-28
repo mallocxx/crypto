f573c9780b411c83b84412efd7a05b7f51800217
magnus <progristillidan@gmail.com>
2025-08-28 19:05:21 +0300

    modified

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,33 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "crypto.h"
+#include <QByteArray>
+#include <cstring>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton, &QPushButton::clicked,
+            this, &MainWindow::onUnlockClicked);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::onUnlockClicked() {
+    QByteArray pass = normalize(ui->lineEdit->text());
+    QByteArray container = QByteArray::fromRawData(
+        reinterpret_cast<const char*>(kContainer),
+        (int)kContainerSize
+        );
+    QByteArray decrypted = decryptContainer(container, pass);
+    if (!decrypted.isEmpty()) {
+        ui->label->setText(QString::fromUtf8(decrypted));
+        std::memset(decrypted.data(), 0, decrypted.size());
+    } else {
+        ui->label->setText("ERROR");
+    }
+    std::memset(pass.data(), 0, pass.size());
 }
